97f7badaba8d799e9d27e071fffdd026e5108c65
Bestial-Dwarf <73252127+Bestial-Dwarf@users.noreply.github.com>
2021-12-18 18:48:17 +0100

    Stworzenie projektu
    Stworzenie projektu i dodanie pod-leveli dla pierwszej mapy.

---
+++ README.md
@@ -0,0 +1,3 @@
+# AntsGame
+
+Developed with Unreal Engine 4
---
+++ Source/AntsGame/AntsGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AntsGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANTSGAME_API AAntsGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
